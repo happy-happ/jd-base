701f4f2ba1510c1084449c84300d006c33014a4e
wuzhi <82073472+wuzhi-docker1@users.noreply.github.com>
2021-04-07 13:55:26 +0800

    update

--- git_pull.sh
+++ git_pull.sh
@@ -23,7 +23,7 @@ ContentNewTask=${ShellDir}/new_task
 ContentDropTask=${ShellDir}/drop_task
 SendCount=${ShellDir}/send_count
 isTermux=${ANDROID_RUNTIME_ROOT}${ANDROID_ROOT}
-ShellURL=${JD_SHELL_URL:-git@gitee.com:shuye72/jd-base.git}
+ShellURL=https://gitee.com/shuye72/jd-base
 ScriptsURL=${JD_SCRIPTS_URL:-git@gitee.com:shuye72/MyActions.git}
 
 ## 更新crontab，gitee服务器同一时间限制5个链接，因此每个人更新代码必须错开时间，每次执行git_pull随机生成。
